000b4115747cd13cd97634a75d6906d39d1681dc
peatchXD <100938925+peatchXD@users.noreply.github.com>
2022-10-28 14:44:32 +0700

    Update Script Fake Level.lua

--- Script Fake Level.lua
+++ Script Fake Level.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(https://raw.githubusercontent.com/peatchXD/-UPDATE-Blox-Fruits-Script-Fake-Level/main/NONAME%20HUB"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/-UPDATE-Blox-Fruits-Script-Fake-Level/main/NONAME%20HUB.lua"))()
